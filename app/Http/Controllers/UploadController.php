849a2b5ed99d432739c9573b8c0b695e1280ec4c
Thomas Lorentzen <thomas@tlorentzen.net>
2022-03-14 11:32:19 +0100

    WIP: slug

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UploadRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UploadController extends Controller
 {
     public function index(UploadRequest $request)
     {
         $token = $request->session()->get('token');
-        $word = $request->get("word");
+        $word = Str::slug($request->get("word"), '_');
 
         if ($request->hasFile('audio_data')) {
             $request->audio_data->storeAs('recordings', date('Ymd-His', time())."-".$token."-".$word.".wav");
