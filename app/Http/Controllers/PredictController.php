3ca23ececfac0ea79618fb7ab0178a3a002a99de
Thomas Lorentzen <thomas@tlorentzen.net>
2022-03-23 15:47:09 +0100

    WIP debug

--- app/Http/Controllers/PredictController.php
+++ app/Http/Controllers/PredictController.php
@@ -29,7 +29,7 @@ class PredictController extends Controller
         if($request->hasFile("wav_file")){
             $name = "predict-".time().".wav";
             $path = $request->wav_file->storeAs('predictions', $name);
-            $wav_file = curl_file_create($path);
+            $wav_file = new CURLFile($path);
 
             $headers = array(
                 'Content-type: multipart/form-data'
